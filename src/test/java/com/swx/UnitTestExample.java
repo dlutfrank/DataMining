d559f2bb6eec5fc12db31a8d11a346a84798f3c5
dlutfrank <dlut.xing@gmail.com.cn>
2016-10-08 10:57:15 +0800

    update unit test demo

--- src/test/java/com/swx/UnitTestExample.java
+++ src/test/java/com/swx/UnitTestExample.java
@@ -3,9 +3,40 @@ package com.swx;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+
 public class UnitTestExample {
+	
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("before class");
+	}
+	
+	@Before
+    public void setUp() throws Exception {
+		System.out.println("before");		
+	}
+	
 	@Test
 	public void testAddExample() {		
-		assertEquals(4,3+1);
+		assertEquals(4,3+1);		
+	}
+	
+	@Test
+	public void testSubExample() {
+		assertEquals(3,5-3);
+	}
+	
+	@After
+	public void after() {
+		System.out.println("after");		
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("after class");
 	}
 }
